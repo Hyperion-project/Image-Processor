Fixed fault in indexing which caused wrong results

ImageProcessor.py
@@ -16,28 +16,30 @@ Image = Image.astype(np.float, copy=False)
 zeroArray = np.zeros((imageDimension[0], imageDimension[1], 1))
 
 # Remove the colors green and blue from the image
-# TODO: See if unsafe can be removed
-np.copyto(Image[:, :, 0:2], zeroArray, "unsafe")
+np.copyto(Image[:, :, 0:2], zeroArray, "safe")
 
 # Create gaussian kernel and apply
 gaussianImage = cv2.GaussianBlur(Image, (3, 3), 0.5)
 
 # Apply threshold and remove negative numbers
-# TODO: Remove negative numbers
 gaussianImage[:, :, 2] -= 170
+gaussianImage[:, :, gaussianImage < 0] = 1.0e-15
 
 Coords = list()
 
 # Run a loop through all the rows and find the maximum. Then use a gaussian approximation to find
 # the sup-pixel offset.
 # TODO: See if this part can be vectorized, should be faster
-for i in range(0, (imageDimension[0] - 1)):
-    index = gaussianImage[:, i, 2].argmax()
-    maxValue = gaussianImage[index, i, 2]
 
-    if maxValue > 0:
-        part1 = np.log(gaussianImage[index - 1, i, 2]) - np.log(gaussianImage[index + 1, i, 2])
-        part2 = np.log(gaussianImage[index - 1, i, 2]) - 2 * np.log(gaussianImage[index, i, 2]) + np.log(
-            gaussianImage[index + 1, i, 2])
+# Get image imagePlane
+imagePlane = gaussianImage[:, :, 2]
+for i in range(0, (imageDimension[0] - 1)):
+    index = imagePlane[i, :].argmax()
+    maxValue = imagePlane[i, index]
+
+    if maxValue > 1.0e-15:
+        part1 = np.log(imagePlane[i, index - 1]) - np.log(imagePlane[i, index + 1])
+        part2 = np.log(imagePlane[i, index - 1]) - 2 * np.log(imagePlane[i, index]) + np.log(
+            imagePlane[i, index + 1])
         delta = 0.5 * (part1/part2)
         Coords.append((i, index+delta))